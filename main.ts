757dd730f8890931a1c81675da9b634c6cac5a8a
Egyed József <jegyed50@gmail.com>
2023-05-07 09:54:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    fenyero = fenyero - 20
+    if (fenyero < 0) {
+        fenyero = 0
+        music.playTone(175, music.beat(BeatFraction.Whole))
+    }
+    led.setBrightness(fenyero)
+})
 input.onSound(DetectedSound.Loud, function () {
     if (lamp == 0) {
         lamp = 1
@@ -19,5 +27,15 @@ input.onSound(DetectedSound.Loud, function () {
             `)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    fenyero = fenyero + 20
+    if (fenyero > 255) {
+        fenyero = 255
+        music.playTone(698, music.beat(BeatFraction.Whole))
+    }
+    led.setBrightness(fenyero)
+})
+let fenyero = 0
 let lamp = 0
 lamp = 0
+fenyero = 124
