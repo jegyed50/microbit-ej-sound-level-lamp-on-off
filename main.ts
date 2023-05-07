fdc41f33eccf0c85b708ca50b547f1cef75c0cde
Egyed József <jegyed50@gmail.com>
2023-05-07 09:40:19 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onSound(DetectedSound.Loud, function () {
+    if (lamp == 0) {
+        lamp = 1
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        lamp = 0
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+let lamp = 0
+lamp = 0
